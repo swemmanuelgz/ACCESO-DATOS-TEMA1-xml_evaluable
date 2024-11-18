822a2062f69b342f9e19395799f5acb85344bfcd
damdu217 <zundercoc@gmail.com>
2024-11-18 16:42:38 +0100

    first commit

---
+++ src/main/java/com/mycompany/xml_evaluable/Xml_evaluable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.xml_evaluable;
+
+/**
+ *
+ * @author damdu217
+ */
+public class Xml_evaluable {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
