df0787ae7660e471571915b18baa378d44b9ec60
damdu217 <zundercoc@gmail.com>
2024-11-18 18:26:09 +0100

    colores

--- src/main/java/com/mycompany/xml_evaluable/view/Xml_evaluable.java
+++ src/main/java/com/mycompany/xml_evaluable/view/Xml_evaluable.java
@@ -15,11 +15,18 @@ import com.mycompany.xml_evaluable.model.RSS;
 public class Xml_evaluable {
 
     public static void main(String[] args) {
+        //Codigos de colores para el console
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_BLACK = "\u001B[30m";
+        String ANSI_RED = "\u001B[31m";
+        String ANSI_GREEN = "\u001B[32m";
+        String ANSI_YELLOW = "\u001B[33m";
+
         System.out.println("Hello World!");
         RssController rssController = new RssController();
         String URL ="https://www.europapress.es/rss/rss.aspx?buscar=inteligencia-artificial";
         RSS rssItem = rssController.getRSS(URL);
-        System.out.println(rssItem.toString());
+        System.out.println(ANSI_GREEN+rssItem.toString()+ANSI_RESET);
         
     }
 }
